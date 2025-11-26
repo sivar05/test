85ea6e9d446a1f47bd0981f0d89780d911105a05
sivar05 <53289136+sivar05@users.noreply.github.com>
2025-11-26 17:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("WELCOME TO MY WORLD!");
+//alert("JavaScript is running!");
+//document.body.innerHTML += "<h2>JavaScript text added!</h2>";
+
+function login() {
+    window.location.href = "login_page.html";
+}
+
+let zoomImages = document.querySelectorAll(".zoom-img");
+let fullscreen = document.getElementById("fullscreen");
+let fullImg = document.getElementById("fullImg");
+
+
+
+
+// Open full screen
+zoomImages.forEach(img => {
+  img.addEventListener("click", function () {
+    fullImg.src = this.src;
+    fullscreen.style.display = "flex";
+  });
+});
+
+
+
+// Click anywhere → close fullscreen
+fullscreen.addEventListener("click", function () {
+  fullscreen.style.display = "none";
+});
+
+
+// ----------- OPEN NEXT PAGE CODE -----------
+let linkImages = document.querySelectorAll(".link-img");
+
+linkImages.forEach(img => {
+  img.addEventListener("click", function () {
+    let page = this.getAttribute("data-link");
+    window.location.href = page;
+  });
+});
