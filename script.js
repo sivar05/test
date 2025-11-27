855b2d7cf11decff08050d92983ade0e68f70be6
sivar05 <siva.rk86@gmail.com>
2025-11-27 11:54:38 +0530

    update

--- script.js
+++ script.js
@@ -40,8 +40,10 @@ linkImages.forEach(img => {
 const images = [
         "img1.jpg",
         "img2.jpg",
-        "img3.jpg"
-        "img4.jpg"
+        "img3.jpg",
+        "img4.jpg",
+        "img5.jpg",
+``
     ];
 
     const gallery = document.getElementById("gallery");
