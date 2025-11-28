1da8537038e27795124e58e8c1dabf71f0a8e9a7
sivar05 <siva.rk86@gmail.com>
2025-11-28 09:48:06 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function signup(){
   window.location.href = "signup.html";
 }
@@ -5,26 +6,20 @@ function signup(){
 function showFull(imgSrc) {
     document.getElementById("fullImg").src = imgSrc;
     document.getElementById("fullscreen").style.display = "flex";
-
-    // hide buttons & other elements
-    document.querySelector(".btn").style.display = "none";
 }
 
 function closeFull() {
     document.getElementById("fullscreen").style.display = "none";
-
-    // show buttons again
-    document.querySelector(".btn").style.display = "block";
 }
 
-//Login function
 function login() {
-            let email = document.getElementById("email").value;
-            let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-            if (email === "" || password === "") {
-                alert("Please fill all fields!");
-                return;
-            }
+    if (email === "" || password === "") {
+        alert("Please fill all fields!");
+        return;
+    }
 
-            alert("Login successful!");
+    alert("Login successful!");
+}
