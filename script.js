efeb29bd8de9f4ed2f76106ae18f1efed7a5dca8
sivar05 <siva.rk86@gmail.com>
2025-12-02 14:14:29 +0530

    update

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ function signup(){
   window.location.href = "../signUp/signup.html";
 }
 
-function showFull(imgSrc) {
-    document.getElementById("fullImg").src = imgSrc;
+
+
+function showFull(src, title, desc) {
     document.getElementById("fullscreen").style.display = "flex";
+    document.getElementById("fullImg").src = src;
+
+    document.getElementById("imgTitle").innerText = title;
+    document.getElementById("imgDesc").innerText = desc;
 }
+
+function closeFull() {
+    document.getElementById("fullscreen").style.display = "none";
+}
+
+
+function closeFull() {
+    document.getElementById("fullscreen").style.display = "none";
+}
+
 
 function closeFull() {
     document.getElementById("fullscreen").style.display = "none";
